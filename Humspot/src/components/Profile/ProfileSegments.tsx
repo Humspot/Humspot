41f4002b530619c76ee63e32d766ad0d8e7d55fe
DAVID YARANON <dyaranon126@gmail.com>
2023-11-29 12:42:43 -0800

    update to profile page

--- Humspot/src/components/Profile/ProfileSegments.tsx
+++ Humspot/src/components/Profile/ProfileSegments.tsx
@@ -13,6 +13,7 @@ import './Profile.css';
 import { formatDate } from "../../utils/formatDate";
 import SkeletonLoading from "../Shared/SkeletonLoading";
 
+
 const ProfileSegments: React.FC = memo(() => {
 
   const context = useContext();
@@ -87,46 +88,44 @@ const ProfileSegments: React.FC = memo(() => {
 
   return (
     <>
-      <div style={{ marginLeft: "2.5%", marginRight: "2.5%", backgroundColor: "var(--ion-background-color)" }}>
-        <IonSegment className="ion-justify-content-center" value={selectedSegment} onIonChange={(e) => { setSelectedSegment(e.detail.value as string) }}>
+      <IonSegment style={{ paddingLeft: "2.5%", paddingRight: "2.5%" }} value={selectedSegment} onIonChange={(e) => { setSelectedSegment(e.detail.value as string) }}>
 
-          <IonSegmentButton value="favorites">
-            <div className="segment-button" style={{ fontSize: "0.8rem" }}>
-              <IonIcon
-                icon={star}
-                style={{ margin: "5%" }}
-                size="large"
-              ></IonIcon>
-              <IonLabel>Favorites</IonLabel>
-            </div>
-          </IonSegmentButton>
+        <IonSegmentButton value="favorites">
+          <div className="segment-button" style={{ fontSize: "0.8rem" }}>
+            <IonIcon
+              icon={star}
+              style={{ margin: "5%" }}
+              size="large"
+            ></IonIcon>
+            <IonLabel>Favorites</IonLabel>
+          </div>
+        </IonSegmentButton>
 
-          <IonSegmentButton value="visited" className="segment-button" style={{ fontSize: "0.8rem" }}>
-            <div className="segment-button">
-              <IonIcon
-                icon={walk}
-                style={{ margin: "5%" }}
-                size="large"
-              ></IonIcon>
-              <IonLabel>Visited</IonLabel>
-            </div>
-          </IonSegmentButton>
+        <IonSegmentButton value="visited" className="segment-button" style={{ fontSize: "0.8rem" }}>
+          <div className="segment-button">
+            <IonIcon
+              icon={walk}
+              style={{ margin: "5%" }}
+              size="large"
+            ></IonIcon>
+            <IonLabel>Visited</IonLabel>
+          </div>
+        </IonSegmentButton>
 
-          <IonSegmentButton value="interactions" className="segment-button" style={{ fontSize: "0.7rem" }}>
-            <div className="segment-button">
-              <IonIcon
-                icon={people}
-                style={{ margin: "5%" }}
-                size="large"
-              ></IonIcon>
-              <IonLabel>Interactions</IonLabel>
-            </div>
-          </IonSegmentButton>
+        <IonSegmentButton value="interactions" className="segment-button" style={{ fontSize: "0.7rem" }}>
+          <div className="segment-button">
+            <IonIcon
+              icon={people}
+              style={{ margin: "5%" }}
+              size="large"
+            ></IonIcon>
+            <IonLabel>Interactions</IonLabel>
+          </div>
+        </IonSegmentButton>
 
-        </IonSegment>
-      </div>
+      </IonSegment>
 
-      {/* <div style={{ height: "1vh", backgroundColor: "var(--ion-background-color)" }} /> */}
+      <div style={{ height: "0.75vh" }} />
 
       <IonContent>
 
@@ -137,104 +136,157 @@ const ProfileSegments: React.FC = memo(() => {
         {selectedSegment === "favorites" ? (
           <>
             {!favoritesLoading && favorites.length === 0 ?
-              <IonTitle className="ion-text-center" style={{ display: "flex", height: "50%" }}>No Favorites</IonTitle>
+              <IonTitle className="ion-text-center" style={{ display: "flex", height: "100%" }}>No Favorites</IonTitle>
               :
-              <IonCard>
-                <IonCardContent className='ion-no-padding'>
-                  <IonList inset={false}>
-                    {!favoritesLoading ?
-                      favorites.map((favorite: HumspotFavoriteResponse, index: number) => {
-                        return (
-                          <FadeIn key={favorite.name + index} delay={index * 50}>
-                            <IonItem className='ion-no-padding' role='button' onClick={() => { if (favorite.activityID) router.push("/activity/" + favorite.activityID) }}>
-                              <IonThumbnail style={{ marginLeft: "10px" }}><img style={{ borderRadius: "5px" }} src={favorite.photoUrl || placeholder} /></IonThumbnail>
-                              <IonLabel style={{ paddingLeft: "10px" }}>
-                                <h2>{favorite.name}</h2>
-                                <div style={{ height: "5px" }} />
-                                <p style={{ fontSize: "0.9rem" }}>{favorite.description}</p>
-                                <p style={{ fontSize: "0.8rem" }}>{favorite.location}</p>
-                              </IonLabel>
-                            </IonItem>
-                          </FadeIn>
-                        )
-                      })
-                      :
-                      <SkeletonLoading count={4} animated={true} height={"5rem"} />
+              <>
+                <IonCard>
+                  <IonCardContent className='ion-no-padding'>
+                    <IonList inset={false}>
+                      {!favoritesLoading ?
+                        favorites.map((favorite: HumspotFavoriteResponse, index: number) => {
+                          return (
+                            <FadeIn key={favorite.name + index} delay={(index % 10) * 50}>
+                              <IonItem className='ion-no-padding' role='button' onClick={() => { if (favorite.activityID) router.push("/activity/" + favorite.activityID) }}>
+                                <IonThumbnail style={{ marginLeft: "10px" }}><img style={{ borderRadius: "5px" }} src={favorite.photoUrl || placeholder} /></IonThumbnail>
+                                <IonLabel style={{ paddingLeft: "10px" }}>
+                                  <h2>{favorite.name}</h2>
+                                  <p style={{ fontSize: "0.9rem" }}>{favorite.description}</p>
+                                  <p style={{ fontSize: "0.8rem" }}>{favorite.location}</p>
+                                </IonLabel>
+                              </IonItem>
+                            </FadeIn>
+                          )
+                        })
+                        :
+                        <SkeletonLoading count={4} animated={true} height={"5rem"} />
+                      }
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+                <IonInfiniteScroll
+                  onIonInfinite={async (ev) => {
+                    if (!context.humspotUser) return;
+                    const response = await handleGetFavoritesGivenUserID(favoritesPageCount, context.humspotUser.userID);
+                    if (!response.success) {
+                      const toast = Toast.create({ message: response.message, duration: 2000, color: 'danger' });
+                      toast.present();
+                    } else {
+                      setFavoritesPageCount((prev) => prev + 1);
+                      setFavorites((prev) => [...prev, ...response.favorites]);
                     }
-                  </IonList>
-                </IonCardContent>
-              </IonCard>
+                    ev.target.complete();
+                  }}
+                >
+                  <IonInfiniteScrollContent></IonInfiniteScrollContent>
+                </IonInfiniteScroll>
+              </>
             }
           </>
         ) : selectedSegment === "visited" ? (
           <>
             {!visitedLoading && visited.length === 0 ?
-              <IonTitle className="ion-text-center" style={{ display: "flex", height: "50%" }}>No Places Visited</IonTitle>
+              <IonTitle className="ion-text-center" style={{ display: "flex", height: "100%" }}>No Places Visited</IonTitle>
               :
-              <IonCard>
-                <IonCardContent className='ion-no-padding'>
-                  <IonList>
-                    {!visitedLoading ?
-                      visited.map((visitedPlace: HumspotVisitedResponse, index: number) => {
-                        return (
-                          <FadeIn key={visitedPlace.name + index} delay={index * 50}>
-                            <IonItem className='ion-no-padding' role='button' onClick={() => { if (visitedPlace.activityID) router.push("/activity/" + visitedPlace.activityID) }}>
-                              <IonThumbnail style={{ marginLeft: "10px" }}><img style={{ borderRadius: "5px" }} src={visitedPlace.photoUrl || placeholder} /></IonThumbnail>
-                              <IonLabel style={{ paddingLeft: "10px" }}>
-                                <h2>{visitedPlace.name}</h2>
-                                <div style={{ height: "5px" }} />
-                                <p style={{ fontSize: "0.9rem" }}>{visitedPlace.description}</p>
-                                <p style={{ fontSize: "0.8rem" }}>{visitedPlace.location}</p>
-                              </IonLabel>
-                            </IonItem>
-                          </FadeIn>
-                        )
-                      })
-                      :
-                      <SkeletonLoading count={4} animated={true} height={"5rem"} />
+              <>
+                <IonCard>
+                  <IonCardContent className='ion-no-padding'>
+                    <IonList inset={false}>
+                      {!visitedLoading ?
+                        visited.map((visitedPlace: HumspotVisitedResponse, index: number) => {
+                          return (
+                            <FadeIn key={visitedPlace.name + index} delay={(index % 10) * 50}>
+                              <IonItem className='ion-no-padding' role='button' onClick={() => { if (visitedPlace.activityID) router.push("/activity/" + visitedPlace.activityID) }}>
+                                <IonThumbnail style={{ marginLeft: "10px" }}><img style={{ borderRadius: "5px" }} src={visitedPlace.photoUrl || placeholder} /></IonThumbnail>
+                                <IonLabel style={{ paddingLeft: "10px" }}>
+                                  <h2>{visitedPlace.name}</h2>
+                                  <p style={{ fontSize: "0.9rem" }}>{visitedPlace.description}</p>
+                                  <p style={{ fontSize: "0.8rem" }}>{visitedPlace.location}</p>
+                                </IonLabel>
+                              </IonItem>
+                            </FadeIn>
+                          )
+                        })
+                        :
+                        <SkeletonLoading count={4} animated={true} height={"5rem"} />
+                      }
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+                <IonInfiniteScroll
+                  onIonInfinite={async (ev) => {
+                    if (!context.humspotUser) return;
+                    const response = await handleGetVisitedGivenUserID(visitedPageCount, context.humspotUser.userID);
+                    if (!response.success) {
+                      const toast = Toast.create({ message: response.message, duration: 2000, color: 'danger' });
+                      toast.present();
+                    } else {
+                      setVisitedPageCount((prev) => prev + 1);
+                      setVisited((prev) => [...prev, ...response.visited]);
                     }
-                  </IonList>
-                </IonCardContent>
-              </IonCard>
+                    ev.target.complete();
+                  }}
+                >
+                  <IonInfiniteScrollContent></IonInfiniteScrollContent>
+                </IonInfiniteScroll>
+              </>
             }
           </>
         ) : (
           <>
-            {!interactionsLoading && interactions.length >= 0 ?
-              <IonTitle className="ion-text-center" style={{ display: "flex", height: "50%" }}>No Interactions <br /> or Comments</IonTitle>
+            {!interactionsLoading && interactions.length === 0 ?
+              <IonTitle className="ion-text-center" style={{ display: "flex", height: "100%" }}>No interactions or Interactions</IonTitle>
               :
-              <IonCard>
-                <IonCardContent className='ion-no-padding'>
-                  <IonList>
-                    {!interactionsLoading ?
-                      interactions.map((interaction: HumspotInteractionResponse, index: number) => {
-                        return (
-                          <FadeIn key={interaction.name + index} delay={index * 50}>
-                            <IonItem className='ion-no-padding' role='button' onClick={() => { if (interaction.activityID) router.push("/activity/" + interaction.activityID) }}>
-                              <IonThumbnail style={{ marginLeft: "10px" }}><img style={{ borderRadius: "5px" }} src={interaction.photoUrl || placeholder} /></IonThumbnail>
-                              <IonLabel style={{ paddingLeft: "10px" }}>
-                                <h2>{interaction.name}</h2>
-                                {interaction.interactionType === 'comment' ?
-                                  <p style={{ fontSize: "0.9rem" }}><b>You commented:</b> {interaction.interactionText}</p>
-                                  :
-                                  <p style={{ fontSize: "0.9rem" }}><b>You RSVP'd</b> for this event</p>
-                                }
-                                <p style={{ fontSize: "0.8rem" }}>{formatDate(interaction.interactionDate as string)}</p>
-                              </IonLabel>
-                            </IonItem>
-                          </FadeIn>
-                        )
-                      })
-                      :
-                      <SkeletonLoading count={4} animated={true} height={"5rem"} />
+              <>
+                <IonCard>
+                  <IonCardContent className='ion-no-padding'>
+                    <IonList inset={false}>
+                      {!interactionsLoading ?
+                        interactions.map((interaction: HumspotInteractionResponse, index: number) => {
+                          return (
+                            <FadeIn key={interaction.name + index} delay={(index % 20) * 50}>
+                              <IonItem className='ion-no-padding' role='button' onClick={() => { if (interaction.activityID) router.push("/activity/" + interaction.activityID) }}>
+                                <IonThumbnail style={{ marginLeft: "10px" }}><img style={{ borderRadius: "5px" }} src={interaction.photoUrl || placeholder} /></IonThumbnail>
+                                <IonLabel style={{ paddingLeft: "10px" }}>
+                                  <h2>{interaction.name}</h2>
+                                  {interaction.interactionType === 'comment' ?
+                                    <p style={{ fontSize: "0.9rem" }}><b>You commented:</b> {interaction.interactionText}</p>
+                                    :
+                                    <p style={{ fontSize: "0.9rem" }}><b>You RSVP'd</b> for this event</p>
+                                  }
+                                  <p style={{ fontSize: "0.8rem" }}>{formatDate(interaction.interactionDate as string)}</p>
+                                </IonLabel>
+                              </IonItem>
+                            </FadeIn>
+                          )
+                        })
+                        :
+                        <SkeletonLoading count={4} animated={true} height={"5rem"} />
+                      }
+                    </IonList>
+                  </IonCardContent>
+                </IonCard>
+                <IonInfiniteScroll
+                  onIonInfinite={async (ev) => {
+                    if (!context.humspotUser) return;
+                    const response = await handleGetInteractionsGivenUserID(interactionsPageCount, context.humspotUser.userID);
+                    if (!response.success) {
+                      const toast = Toast.create({ message: response.message, duration: 2000, color: 'danger' });
+                      toast.present();
+                    } else {
+                      setInteractionsPageCount((prev) => prev + 1);
+                      setInteractions((prev) => [...prev, ...response.interactions]);
                     }
-                  </IonList>
-                </IonCardContent>
-              </IonCard>
+                    ev.target.complete();
+                  }}
+                >
+                  <IonInfiniteScrollContent></IonInfiniteScrollContent>
+                </IonInfiniteScroll>
+              </>
             }
           </>
         )
         }
+        <br /><br/><br /><br/><br /><br/><br /><br/><br /><br/><br /><br/><br /><br/>
       </IonContent>
     </>
   );
