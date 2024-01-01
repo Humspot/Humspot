af6be8c74439ac9799e694ec8aeefee45c711337
DAVID YARANON <dyaranon126@gmail.com>
2023-12-31 17:03:41 -0800

    search page now includes tags that are similar to the search query

--- Humspot/src/pages/Search.tsx
+++ Humspot/src/pages/Search.tsx
@@ -1,25 +1,287 @@
-
-import { useCallback, useEffect, useRef, useState } from "react";
+/**
+ * @file Search.tsx
+ * @fileoverview Search results page for when users run a query using the searchbar on the Explore page.
+ */
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import {
-  IonButton, IonButtons, IonCard, IonCardTitle, IonContent, IonHeader, IonIcon,
-  IonInfiniteScroll, IonInfiniteScrollContent, IonPage, IonSearchbar,
-  IonSkeletonText, IonText, IonTitle, IonToolbar, useIonRouter
-} from "@ionic/react";
-import { useParams } from "react-router-dom";
-import { chevronBackOutline, shareOutline } from "ionicons/icons";
-import { Share } from "@capacitor/share";
-import FadeIn from "@rcnoverwatcher/react-fade-in-react-18/src/FadeIn";
+  IonButton, IonButtons, IonCard, IonCardTitle, IonChip, IonContent, IonHeader, IonIcon,
+  IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonPage, IonSearchbar,
+  IonSkeletonText, IonText, IonTitle, IonToolbar, useIonRouter, useIonViewWillEnter
+} from '@ionic/react';
+import { useParams } from 'react-router-dom';
+import { chevronBackOutline, shareOutline } from 'ionicons/icons';
+import { Share } from '@capacitor/share';
+import FadeIn from '@rcnoverwatcher/react-fade-in-react-18/src/FadeIn';
 
 import placeholder from '../assets/images/school_placeholder.jpeg';
-import { handleGetSearchResults } from "../utils/server";
-import { formatDate } from "../utils/functions/formatDate";
-import { Keyboard } from "@capacitor/keyboard";
-import { timeout } from "../utils/functions/timeout";
+import { handleGetSearchResults } from '../utils/server';
+import { formatDate } from '../utils/functions/formatDate';
+import { Keyboard } from '@capacitor/keyboard';
+import { timeout } from '../utils/functions/timeout';
+import { useContext } from '../utils/hooks/useContext';
 
 type SearchParams = {
   query: string;
 };
+const tags: string[] = [
+  'Camping',
+  'Chill',
+  'Cultural',
+  'Fishing',
+  'Food',
+  'Highlight',
+  'Hiking',
+  'Historic',
+  'Kayaking',
+  'Museum',
+  'Networking',
+  'Photography',
+  'Scenic',
+  'School',
+  'Shopping',
+  'Sightseeing',
+  'Wildlife',
+  'Beach',
+  'Hiking',
+  'Camping',
+  'Fishing',
+  'Sightseeing',
+  'Swimming',
+  'Kayaking',
+  'Canoeing',
+  'Boating',
+  'Biking',
+  'Hunting',
+  'Wildlife',
+  'Birding',
+  'Photography',
+  'Scenic',
+  'Historic',
+  'Museum',
+  'Cultural',
+  'Shopping',
+  'Winery',
+  'Brewery',
+  'Distillery',
+  'Golf',
+  'Skiing',
+  'Snowboarding',
+  'Snowmobiling',
+  'Snowshoeing',
+  'Cross Country Skiing',
+  'Fun',
+  'Adventure',
+  'Relax',
+  'Chill',
+  'Music',
+  'Festival',
+  'Food',
+  'School',
+  'Educational',
+  'Cultural',
+  'Art',
+  'Dance',
+  'Nature',
+  'Outdoor',
+  'Indoor',
+  'Fitness',
+  'Yoga',
+  'Meditation',
+  'Technology',
+  'Science',
+  'Networking',
+  'Business',
+  'Entrepreneurship',
+  'Community',
+  'Charity',
+  'Volunteering',
+  'Health',
+  'Wellness',
+  'Cooking',
+  'Baking',
+  'Crafts',
+  'DIY',
+  'Workshop',
+  'Lecture',
+  'Seminar',
+  'Conference',
+  'TradeShow',
+  'Exhibition',
+  'Theatre',
+  'Cinema',
+  'Movies',
+  'Comedy',
+  'Drama',
+  'Romance',
+  'SciFi',
+  'Fantasy',
+  'Horror',
+  'Thriller',
+  'Mystery',
+  'Family',
+  'Kids',
+  'Teens',
+  'Adults',
+  'Seniors',
+  'Pets',
+  'Animals',
+  'Gardening',
+  'Environment',
+  'Sustainability',
+  'Politics',
+  'History',
+  'Literature',
+  'Poetry',
+  'Writing',
+  'Journalism',
+  'Photography',
+  'Film',
+  'Animation',
+  'VideoGames',
+  'Esports',
+  'BoardGames',
+  'CardGames',
+  'RolePlaying',
+  'Fashion',
+  'Beauty',
+  'Makeup',
+  'Skincare',
+  'Haircare',
+  'Shopping',
+  'Auction',
+  'Sale',
+  'Fundraising',
+  'Donations',
+  'Spirituality',
+  'Religion',
+  'Philosophy',
+  'Astrology',
+  'Travel',
+  'Tourism',
+  'Sports',
+  'Snow',
+  'Automotive',
+  'Cycling',
+  'Running',
+  'Abbey of the Redwoods',
+  'Adel\'s Restaurant',
+  'Adventure',
+  'Arcata Community Center',
+  'Arcata Playhouse',
+  'Arcata Plaza',
+  'Arcata Theatre Lounge',
+  'Arkley Center for the Performing Arts',
+  'Azalea Hall',
+  'Bayshore Mall',
+  'Beach',
+  'Bear River Casino Resort',
+  'Benbow Historic Inn',
+  'Blue Lake Roller Rink',
+  'Brewery',
+  'Broadway Cinema',
+  'Cal Poly Humboldt',
+  'California State Polytechnic University, Humboldt',
+  'Central Station Sports Bar',
+  'Chill',
+  'City of Eureka',
+  'Clarke Historical Museum',
+  'College of the Redwoods',
+  'Crystalline Collective',
+  'Del Norte County Fairgrounds',
+  'Dick Taylor Craft Chocolate',
+  'Dow\'s Prairie Grange',
+  'Dutch Bros Coffee',
+  'Entrepreneurship',
+  'Environmental Protection Information Center',
+  'Eureka Center for Spiritual Living',
+  'Eureka Chamber of Commerce',
+  'Eureka Community Services/Adorni Center',
+  'Eureka Main Library',
+  'Eureka the Pentecostal Church',
+  'Ferndale Music Company & The Old Steeple',
+  'Ferndale Repertory Theatre',
+  'First Baptist Church of Fortuna',
+  'Fortuna Library',
+  'Fortuna Monday Club',
+  'Garberville Farmers\' Market',
+  'Gene Lucas Community Center',
+  'Gyppo Ale Mill',
+  'Halvorsen Park',
+  'Harbor Lanes',
+  'HealthSPORT Gymnastics',
+  'Healy Senior Center',
+  'Herb & Market Humboldt',
+  'Highlight',
+  'Historic Eagle House',
+  'HSU',
+  'Humboldt Bay Aquatic Center',
+  'Humboldt Bay Social Club',
+  'Humboldt Brews LLC',
+  'Humboldt County Board of Supervisors',
+  'Humboldt County Library',
+  'Humboldt County Office of Education',
+  'Inn at 2nd & C',
+  'Jacoby\'s Storehouse',
+  'Labor Temple',
+  'Las Michoacana\'s',
+  'Lavender Rose Paint Nights',
+  'Mad River Brewing Co',
+  'Mad River Grange',
+  'Mateel Community Center',
+  'Mattole Valley Resource Center',
+  'McKay Community Forest',
+  'McKinleyville Glass',
+  'Minor Theatre',
+  'Morris Graves Museum of Art',
+  'New Heart Community Church',
+  'North Coast Aikido',
+  'North Coast Growers\' Association Farmers Market',
+  'North Coast Repertory Theatre',
+  'Northern Realms Trading Cards',
+  'Official Crescent City - Del Norte County Visitor ',
+  'Pacific Union School',
+  'Prairie Creek Redwoods State Park',
+  'Redway Elementary School',
+  'Redwood Acres Raceway',
+  'Redwood Curtain Brewing Company - Eureka, Myrtleto',
+  'Redwood Discovery Museum',
+  'Redwood Gun Club',
+  'Redwood Raks World Dance Collective',
+  'Redwood Retro',
+  'Redwood Sky Walk at Sequoia Park Zoo',
+  'Richards\' Goat Tavern & Tea Room',
+  'River Lodge Conference Center',
+  'Samoa Power Pole Beach',
+  'Sapphire Palace Event Center',
+  'Savage Henry Comedy Club',
+  'School',
+  'Seawood Cape Preserve',
+  'Septentrio Winery',
+  'Sequoia Park',
+  'Shelter Cove Chapel By the Sea',
+  'Six Rivers Brewery',
+  'Southern Humboldt Chamber of Commerce & Visitors',
+  'Stone Junction',
+  'The Arcata Plaza',
+  'The Arcata Veterans Memorial Building',
+  'The Basement',
+  'The Bigfoot Museum',
+  'The Epitome Gallery',
+  'The Eureka Theater',
+  'The Game Zone',
+  'The Jam',
+  'The Lutheran Church of Arcata',
+  'the Sanctuary',
+  'Theater Arts Building/Van Duzer Theatr',
+  'Thomas Carr Park - Firefighters Pavilion',
+  'Timber Heritage Association',
+  'TrinidadHealingArts',
+  'United Indian Health Services - Potawot Health Vil',
+  'United Methodist Church-Joyful',
+  'Wave Lounge',
+  'Wharfinger Building and Eureka Public Marina',
+]
 
 const LoadingCard = () => {
   return (
@@ -31,7 +293,7 @@ const LoadingCard = () => {
       </div>
     </IonCard >
   )
-}
+};
 
 const Search = () => {
 
@@ -39,6 +301,8 @@ const Search = () => {
   const query: string = params.query ?? '';
 
   const router = useIonRouter();
+  const context = useContext();
+  const contentRef = useRef<HTMLIonContentElement | null>(null);
   const searchRef = useRef<HTMLIonSearchbarElement | null>(null);
 
   const [loading, setLoading] = useState<boolean>(true);
@@ -47,9 +311,9 @@ const Search = () => {
 
   const handleShare = async () => {
     await Share.share({
-      text: "Check out these activities on Humspot!",
-      title: "Check out these activities on Humspot!",
-      url: "https://humspotapp.com" + window.location.pathname
+      text: 'Check out these activities on Humspot!',
+      title: 'Check out these activities on Humspot!',
+      url: 'https://humspotapp.com' + window.location.pathname
     });
   };
 
@@ -77,6 +341,11 @@ const Search = () => {
     handleSearch();
   }, [handleSearch]);
 
+  useIonViewWillEnter(() => {
+    context.setShowTabs(false);
+  }, [])
+
+
   return (
     <IonPage>
       <IonHeader>
@@ -89,25 +358,44 @@ const Search = () => {
               <IonIcon icon={chevronBackOutline} /> <p>Back</p>
             </IonButton>
           </IonButtons>
-          <IonButtons slot="end">
+          <IonButtons slot='end'>
             <IonButton onClick={handleShare}>
-              <IonIcon style={{ transform: "scale(1.1)" }} icon={shareOutline} />
+              <IonIcon style={{ transform: 'scale(1.1)' }} icon={shareOutline} />
             </IonButton>
           </IonButtons>
         </IonToolbar>
         <IonToolbar>
           <IonSearchbar
             ref={searchRef}
-            // onClick={() => contentRef && contentRef.current && contentRef.current.scrollToTop(1000)}
-            placeholder="Search for Activities" spellcheck={true}
-            type="search" enterkeyhint="search"
-            autocorrect="off" showCancelButton="focus" animated={true}
+            onClick={() => contentRef && contentRef.current && contentRef.current.scrollToTop(1000)}
+            placeholder='Search for Activities' spellcheck={true}
+            type='search' enterkeyhint='search'
+            autocorrect='off' showCancelButton='focus' animated={true}
             onKeyDown={e => isEnterPressed(e.key)}
           />
         </IonToolbar>
       </IonHeader>
 
-      <IonContent>
+      <IonContent ref={contentRef}>
+        {query.length > 2 &&
+          <>
+            <div style={{ paddingLeft: '10px', paddingTop: '7.5px', paddingBottom: '0px', marginBottom: '0px' }}>
+              {tags.map((tag: string, idx: number) => {
+                if (tag.toLowerCase().includes(query.toLowerCase())) {
+                  return (
+                    <IonChip
+                      key={tag + idx}
+                      onClick={() => router.push('/more-results/' + encodeURIComponent(tag))}
+                      color='secondary'
+                    >
+                      <IonLabel>{tag}</IonLabel>
+                    </IonChip>
+                  )
+                }
+              })}
+            </div>
+          </>
+        }
         {
           loading ?
             <>
